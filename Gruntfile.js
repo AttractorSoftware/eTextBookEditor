541206c9c05093bcee7de6b5931650d369b8c5ad
manasovd <afroprogger@gmail.com>
2014-07-08 16:00:24 +0600

    #14   Исправил ошибки с директориями при тестах, оптимизировал грант

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,69 +1,60 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     grunt.initConfig({
         concat: {
             main: {
                 src: [
                     'js/lib/jquery-2.1.1.min.js'
-                    ,'js/lib/bootstrap.min.js'
-                    ,'js/lib/underscore-min.js'
-                    ,'js/lib/backbone-min.js'
-                    ,'js/app.js'
-                    ,'js/eTextBook/*.js'
-                    ,'js/eTextBook/inline/*.js'
-                    ,'js/eTextBook/widget/*.js'
+                    , 'js/lib/bootstrap.min.js'
+                    , 'js/lib/underscore-min.js'
+                    , 'js/lib/backbone-min.js'
+                    , 'js/app.js'
+                    , 'js/eTextBook/*.js'
+                    , 'js/eTextBook/inline/*.js'
+                    , 'js/eTextBook/widget/*.js'
                 ],
                 dest: 'js/script.js'
-            }
-            ,template: {
+            }, template: {
                 src: [
                     'js/lib/jquery-2.1.1.min.js'
-                    ,'js/lib/underscore-min.js'
-                    ,'js/lib/backbone-min.js'
-                    ,'js/app.js'
-                    ,'js/eTextBook/widget.js'
-                    ,'js/eTextBook/widgetRepository.js'
-                    ,'js/eTextBook/widget/*.js'
-                    ,'js/html5Player.js'
+                    , 'js/lib/underscore-min.js'
+                    , 'js/lib/backbone-min.js'
+                    , 'js/app.js'
+                    , 'js/eTextBook/widget.js'
+                    , 'js/eTextBook/widgetRepository.js'
+                    , 'js/eTextBook/widget/*.js'
+                    , 'js/html5Player.js'
                 ],
                 dest: 'template/js/script.js'
             }
-        }
-        ,concat_css: {
+        }, concat_css: {
             main: {
                 src: [
                     'css/bootstrap.min.css'
-                    ,'css/style.css'
-                ]
-                ,dest: 'css/main-style.css'
-            }
-            ,template: {
+                    , 'css/style.css'
+                ], dest: 'css/main-style.css'
+            }, template: {
                 src: [
                     'css/bootstrap.min.css'
-                    ,'template/css/style.css'
-                    ,'css/html5Player.css'
-                ]
-                ,dest: 'template/css/main-style.css'
+                    , 'template/css/style.css'
+                    , 'css/html5Player.css'
+                ], dest: 'template/css/main-style.css'
             }
-        }
-        ,cssmin: {
+        }, cssmin: {
             main: {
                 files: {
                     'css/main-style.min.css': ['css/main-style.css']
                 }
-            }
-            ,template: {
+            }, template: {
                 files: {
                     'template/css/main-style.min.css': ['template/css/main-style.css']
                 }
             }
-        }
-        ,uglify: {
+        }, uglify: {
             main: {
                 files: {
                     'js/script.min.js': '<%= concat.main.dest %>'
                 }
-            }
-            ,template: {
+            }, template: {
                 files: {
                     'template/js/script.min.js': '<%= concat.template.dest %>'
                 }
@@ -71,11 +62,9 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-concat-css');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
 
-    grunt.registerTask('default', ['concat', 'concat_css', 'cssmin', 'uglify']);
+    grunt.registerTask('default', ['newer:concat', 'newer:concat_css', 'newer:cssmin', 'newer:uglify']);
 
-}
+};
