1155079ea4ebd57178601369c0dfff5826bba703
Alex Shedrin <alex.shedrin.ne@gmail.com>
2014-07-24 15:37:37 +0600

    #21 удалил из репозитория не нужные файлы

--- deploy.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $print = '';
-    $print .= 'Start deploy <br />';
-    $print .= 'Current directory: '. shell_exec('echo $PWD') . '<br />';
-    $print .= shell_exec('git pull 2>&1');
-    $print .= shell_exec('bash grunt-build.sh');
-    print_r($print);
-?>
--- remove-file.php
+++
@@ -1,3 +0,0 @@
-<?php
-    require_once 'lib/Util.class.php';
-    unlink(Util::getRootDir() . $_POST['file']);
