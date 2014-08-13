c17cfd529c610584cd2d5a16750c5b23c3e9d205
Alex Shedrin <alex.shedrin.ne@gmail.com>
2014-08-13 10:53:44 +0600

    #24 поменял стиль отображения в списке книг

--- web/js/eTextBook/bookForm.js
+++ web/js/eTextBook/bookForm.js
@@ -24,6 +24,7 @@ var bookForm = function() {
             });
             $this.alertHide();
             $this.modal.find('.modal-footer .btn-primary').show();
+            $this.modal.find('.cover-view').attr({ style: '' });
         });
     }
 
