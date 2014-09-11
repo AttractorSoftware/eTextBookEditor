e296262dfffad244e1b0739f3de892ebd8b1ab9b
manasovd <afroprogger@gmail.com>
2014-09-11 11:03:13 +0600

    #5 Добавил виджет "тест"

--- web/js/eTextBook/widget/test.js
+++ web/js/eTextBook/widget/test.js
@@ -6,33 +6,72 @@ var eTextBookWidgetTest = eTextBookWidget.extend({
     },
 
     startEdit: function () {
-        for (var i = 0; i < this.cont.find('.task').length; i++) {
-            var item = $(this.cont.find('.test-widget .task')[i]);
-            item.find('input').val(item.find('view-element').html());
-            item.find('select').val(item.attr('value'));
+        var items = this.cont.find('.task');
+        for (var i = 0; i < items.length; i++) {
+            var item = $(items[i]);
+            item.find('.question input').val(item.find('view-element h5').text());
+            item.find('.choices-list').html(item.find('view-element .choices-list').html());
+        }
+    },
+
+    finishEdit: function () {
+        var items = this.cont.find('.task');
+        for (var i = 0; i < items.length; i++) {
+            var item = $(items[i]);
+            item.find('view-element')
+                .attr('data-right-answer-ID', item.find('.choices-list input:checked').attr('id'))
+                .append(item.find('.choices-list').clone());
+            item.find('view-element h5').html(item.find('.question input').val())
         }
     },
 
     activate: function () {
         this.cont = this.editCont.find('.test-widget');
-        this.addQuestionWithChoice(this.cont);
+        this.addTask();
         this.binds();
     },
 
-    addQuestionWithChoice: function (obj) {
-        obj.append(App.eTextBookTemplate.getTemplate('testWidgetTask'));
-        obj.find('.question').html(App.eTextBookTemplate.getTemplate('emptyQuestion'));
-        this.addChoice(obj.find('.add-question .choices:last'));
+    addEditElements: function () {
+        var $tasks = this.cont.find('.task');
+        for (var i = 0; i < $tasks.length; i++) {
+            var $task = $($tasks[i]);
+            if (!$task.find('edit-element').length)
+                $task.append(App.eTextBookTemplate.getTemplate('testWidgetEditElement'));
+        }
     },
 
-    addChoice: function (obj) {
-        obj.append(App.eTextBookTemplate.getTemplate('addChoice'));
+    addTask: function () {
+        this.addEditElements();
+        var input = this.cont.find('.question input').last(),
+            inputIsNotEmpty = input.val() !== '';
+        if (inputIsNotEmpty) {
+            this.cont.find('.question .glyphicon').last().addClass('glyphicon-remove')
+                .removeClass('add glyphicon-plus');
+            this.cont.append(App.eTextBookTemplate.getTemplate('testWidgetTask'));
+            this.addEditElements();
+        }
+        else {
+            input.focus();
+        }
     },
 
-    parseQuestion: function (obj) {
-        var value = obj.children('input').val();
-        obj.html(App.eTextBookTemplate.getTemplate('editedQuestion'));
-        obj.children('h5').html(value).show("normal");
+    parseChoice: function (obj) {
+        var $choices = obj.closest('.choices'),
+            $input = $choices.find('.add-choice input'),
+            value = $input.val(),
+            inputIsNotEmpty = value !== '';
+
+        if (inputIsNotEmpty) {
+            var uid = App.eTextBookUtils.generateUID();
+            $choices.find('.choices-list').append(App.eTextBookTemplate.getTemplate('choiceRadioButton'));
+            $choices.find('.choiceRadioButton:last').attr('id', uid).attr('class', uid);
+            $choices.find('label:last').attr('for', uid).html(value);
+            $input.val('');
+        }
+        else {
+            $input.focus();
+        }
+
     },
 
     binds: function () {
@@ -41,36 +80,50 @@ var eTextBookWidgetTest = eTextBookWidget.extend({
 
         $('.test-widget')
             .on('click', '.question .add', function () {
-                $this.parseQuestion($(this).closest('.question'));
-                addElement.call(this);
+                $this.addTask();
             })
             .on('click', '.add-choice .add', function () {
-                addElement.call(this);
+                $this.parseChoice($(this));
             })
             .on('keyup', '.question input', function (e) {
-                if (e.which == ENTER_KEY) addElement.call(this);
+                if (e.which === ENTER_KEY) {
+                    $this.addTask();
+                }
             })
             .on('keyup', '.add-choice input', function (e) {
-                if (e.which == ENTER_KEY) addElement.call(this);
+                if (e.which === ENTER_KEY) $this.parseChoice($(this));
             })
             .on('click', '.glyphicon-remove', function () {
-                $(this).closest('edit-element').remove();
+                $(this).closest('.delete-point').remove();
+            })
+
+            .on('change', '.choice input', function () {
+                $('.' + $(this).attr('class')).prop('checked', true);
             });
+    },
 
-        function addElement() {
-            var closestEditElement = $(this).closest('edit-element');
-            var closestGlyphIcon = closestEditElement.find('.glyphicon').first();
-
-//            if (closestGlyphIcon.hasClass("add")) {
-                if (closestEditElement.hasClass("add-question"))
-                    $this.addQuestionWithChoice($(this).closest(".test-widget"));
-                else if (closestEditElement.hasClass("add-choice"))
-                    $this.addChoice($(this).closest(".choices"));
-                closestGlyphIcon.addClass('glyphicon-remove').removeClass('add glyphicon-plus');
-//            }
-        }
-
+    viewActivate: function () {
+        this.contentContainer.find('.choice input').bind('change', function () {
+            var task = $(this).closest('view-element');
+            var selectedID = $(this).attr('id');
+            console.log('right here');
+            var rightID = task.attr('data-right-answer-id');
+            console.log(rightID + ' and selected ' + selectedID);
+            if (rightID === '') {
+                task.removeClass('failed');
+                task.removeClass('success');
+            } else {
+                if (selectedID !== rightID) {
+                    task.addClass('failed');
+                    task.removeClass('success');
+                } else {
+                    task.removeClass('failed');
+                    task.addClass('success');
+                }
+            }
+        });
     }
+
 
 });
 
